f40c4a0da18943c3d6c8d1943734bb0ce875dc0f
Alixandrinoj7 <alixandrinoj7@gmail.com>
2025-06-05 12:25:12 -0300

    Update config.js

--- firebase/config.js
+++ firebase/config.js
@@ -1,1 +1,13 @@
-// Firebase configuration ficará aqui depois
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+// Configurações do Firebase, puxando das variáveis de ambiente
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
