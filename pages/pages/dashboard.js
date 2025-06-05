fe800da063e598904d004f8c4f4e0ab1fc3ea7fa
Alixandrinoj7 <alixandrinoj7@gmail.com>
2025-06-05 12:13:30 -0300

    Create dashboard.js

---
+++ pages/pages/dashboard.js
@@ -0,0 +1,10 @@
+import SignalList from '../components/SignalList';
+
+export default function Dashboard() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Dashboard de Sinais</h1>
+      <SignalList />
+    </div>
+  );
+}
