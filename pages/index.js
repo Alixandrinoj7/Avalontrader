e39c54160b9a7cec1a0f8e6b7fdaf0a5481f87da
Alixandrinoj7 <alixandrinoj7@gmail.com>
2025-06-05 12:12:56 -0300

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>AvalonTrader</h1>
+      <p>Sinais de trading 24h com IA</p>
+      <a href="/dashboard">Ir para Dashboard</a>
+    </div>
+  );
+}
